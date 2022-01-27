681d61e38cc6b7b747d3dd1eb4f08c8aabe6e314
jacek <jacektheboy@gmail.com>
2022-01-27 08:54:54 -0500

    Just work

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] temp = {32, 29, 10, 22, 41, 28, 33};
+        int[] per = {95, 60, 25, 5, 0, 7, 90};
+        String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+        if (temp[0] <= 32)  { if (per[0] > 50){
+            System.out.println(days[0]);
+        }}
+        if (temp[1] <= 32)  { if (per[1] > 50){
+            System.out.println(days[1]);
+        }}
+        if (temp[2] <= 32)  { if (per[2] > 50){
+            System.out.println(days[2]);
+        }}
+        if (temp[3] <= 32)  { if (per[3] > 50){
+            System.out.println(days[3]);
+        }}
+        if (temp[4] <= 32)  { if (per[4] > 50){
+            System.out.println(days[4]);
+        }}
+        if (temp[5] <= 32)  { if (per[5] > 50){
+            System.out.println(days[5]);
+        }}
+        if (temp[6] <= 32)  { if (per[6] > 50){
+            System.out.println(days[6]);
+        }}
+
+
+    }
+}
